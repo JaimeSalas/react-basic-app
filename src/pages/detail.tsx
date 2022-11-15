36030524bfb2fb711756ee1cd038e24ce23f6417
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-15 12:52:59 +0100

    added detail

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -20,13 +20,61 @@ git remote add origin git@github.com:JaimeSalas/react-basic-app.git
 git push -u origin main
 */
 
+interface DetailApi {
+  id: number;
+  login: string;
+  name: string;
+  company: string;
+  bio: string;
+}
+
+interface DetailEntity {
+  id: string;
+  login: string;
+  name: string;
+  company: string;
+  bio: string;
+}
+
+const mapDetailEntity = (detail: DetailApi): DetailEntity => ({
+  ...detail,
+  id: detail.id.toString()
+});
+
+const initDetail = (): DetailEntity => ({
+  id: '',
+  bio: '',
+  company: '',
+  login: '',
+  name: '',
+}); 
+
 export const DetailPage: React.FC = () => {
   const { id } = useParams(); 
+  const [detail, setDetail] = React.useState<DetailEntity>(initDetail);
+
+  React.useEffect(() => {
+    
+    fetch(`https://api.github.com/users/${id}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+      })
+      .then(mapDetailEntity)
+      .then(setDetail)
+      .catch(console.error);
+
+  }, []);
 
   return (
       <>
         <h2>DETAIL PAGE</h2>
-        <h3>User Id: {id}</h3>
+        <p>id: {detail.id}</p>
+        <p>login: {detail.login}</p>
+        <p>name: {detail.name}</p>
+        <p>company: {detail.company}</p>
+        <p>bio: {detail.bio}</p>
         <Link to='/list'>Back to list page</Link>
       </>
   );
